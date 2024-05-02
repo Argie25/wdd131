dbb6de2b28fa772e1b2055ffb73fe79a711c6063
Argie25 <argiedc888@gmail.com>
2024-05-02 14:49:46 +0800

    week2

--- week01/script.js
+++ week01/script.js
@@ -11,10 +11,10 @@ function createParagraph() {
   }
 
       // Get the last modified date of the document
-      var lastModifiedDate = document.lastModified;
+      let lastModifiedDate = document.lastModified;
 
       // Find the paragraph element with the id "lastModified"
-      var paragraphElement = document.getElementById("lastModified");
+      let paragraphElement = document.getElementById("lastModified");
   
       // Set the text content of the paragraph element to display the last modified date
       paragraphElement.textContent = "Last modified: " + lastModifiedDate;

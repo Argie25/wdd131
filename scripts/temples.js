9810c7eaf6149ca802784add1886041ed2b21a87
Argie25 <argiedc888@gmail.com>
2024-05-08 14:50:10 +0800

    done

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,10 +7,10 @@ hamburger.addEventListener('click', () => {
 });
 
 
-//    // Get the last modified date of the document
-// let lastModifiedDate = document.lastModified;
-//    // Find the paragraph element with the id "lastModified"
-// let paragraphElement = document.getElementById("lastModified");
-//    // Set the text content of the paragraph element to display the last modified date
-// paragraphElement.textContent = "Last modified: " + lastModifiedDate;
+   // Get the last modified date of the document
+let lastModifiedDate = document.lastModified;
+   // Find the paragraph element with the id "lastModified"
+let paragraphElement = document.getElementById("lastModified");
+   // Set the text content of the paragraph element to display the last modified date
+paragraphElement.textContent = "Last modified: " + lastModifiedDate;
 

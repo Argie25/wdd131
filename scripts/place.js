03f6ce5f7320921ae7bc15d30204baa524bf59ef
Argie25 <argiedc888@gmail.com>
2024-05-19 14:58:24 +0800

    none

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+   // Get the last modified date of the document
+   let lastModifiedDate = document.lastModified;
+   // Find the paragraph element with the id "lastModified"
+let paragraphElement = document.getElementById("lastModified");
+   // Set the text content of the paragraph element to display the last modified date
+paragraphElement.textContent = "Last modified: " + lastModifiedDate;
+
